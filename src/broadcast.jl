53841195aa3a7a81f6ccc246d9c4661f0392fe33
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-07-02 01:21:59 +0100

    small changes to broadcasting

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -15,21 +15,20 @@ promote_containertype(::Type{FTField}, ::Type{Array}) = FTField
 promote_containertype(::Type{Array}, ::Type{FTField}) = FTField
 
 # extract underlying matrices and broadcast over that
-@generated function broadcast_c!(f, ::Type{FTField}, ::Type{FTField}, dest, src...)
-    rest = Expr(:tuple, ntuple(k -> :(unsafe_get(src[$k])), length(src))...)
+@generated function broadcast_c!(f, ::Type{FTField}, ::Type{FTField}, dest, src::Vararg{Any, N}) where N
+    args = [:(unsafe_get(src[$k])) for k = 1:N]
     quote
-        broadcast!(f, unsafe_get(dest), $rest...)
+        broadcast!(f, unsafe_get(dest), $(args...))
     end
 end
 
 # allocating version
-@generated function broadcast_c(f, ::Type{FTField}, src...)
-    rest = Expr(:tuple, ntuple(k -> :(unsafe_get(src[$k])), length(src))...)
+@generated function broadcast_c(f, ::Type{FTField}, src::Vararg{Any, N}) where N
+    args = [:(unsafe_get(src[$k])) for k = 1:N]
     quote
-        FTField(broadcast(f, $rest...))
+        FTField(broadcast(f, $(args...)))
     end
 end
 
 # extract the underlying matrix representation 
-@inline Base.unsafe_get(U::FTField) = U.data
-@inline Base.unsafe_get(op::DiffOperator) = op.data
+@inline Base.unsafe_get(U::Union{FTField, DiffOperator}) = U.data
