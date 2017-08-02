87284f1f8b57ab1c4a40d3b33223cf2b71a17aab
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-02 11:08:13 +0100

    remove stuff from run tests

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -1,16 +1,3 @@
-# a complex exponential and its derivatives
-fun(x, y, c::Number, j::Integer, k::Integer) = 
-    0.5.*real(c.*exp.(im*(j.*x .+ k.*y)) .+ conj(c).*exp.(-im*(j.*x .+ k.*y)))
-
-funx(x, y, c::Number, j::Integer, k::Integer) = 
-    0.5.*real(im.*j.*c.*exp.(im*(j.*x .+ k.*y)) .- im.*j.*conj(c).*exp.(-im*(j.*x .+ k.*y)))
-
-funy(x, y, c::Number, j::Integer, k::Integer) =     
-    0.5.*real(im.*k.*c.*exp.(im*(j.*x .+ k.*y)) .- im.*k.*conj(c).*exp.(-im*(j.*x .+ k.*y)))        
-
-funxx(x, y, c::Number, j::Integer, k::Integer) = -j^2*fun(x, y, c, j, k)
-funyy(x, y, c::Number, j::Integer, k::Integer) = -k^2*fun(x, y, c, j, k)
-
 # an useful macro for printing arrays
 macro display(ex)
     quote
