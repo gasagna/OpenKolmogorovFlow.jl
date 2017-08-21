a4787f80b0617a84cc0d7fec40151107a901c24c
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-21 21:19:22 +0100

    fix input types

--- tests/test_tangent.jl
+++ tests/test_tangent.jl
@@ -3,15 +3,15 @@ using OpenKolmogorovFlow
 using Base.Test
 
 @testset "field creation and broadcast           " begin
-    U = FTField(4, Complex{VarNum{Float64}})
-    u = Field(4,           VarNum{Float64})
+    U = FTField(4, VarNum{Float64})
+    u = Field(4, VarNum{Float64})
     @test eltype(U) == Complex{VarNum{Float64}}
     @test eltype(u) == VarNum{Float64}
 end
 
 @testset "FTField perturbation                   " begin
     @testset "set perturbation                       " begin
-        U = FTField(4, Complex{VarNum{Float64}})
+        U = FTField(4, VarNum{Float64})
         p = FTField(randn(4, 3) + im*randn(4, 3))
         set_pert!(U, p)
         @test pert.(real.(U.data)) == real.(p.data)
@@ -22,7 +22,7 @@ end
     @testset "get perturbation                       " begin
         U_ = (randn(4, 3) + δ*randn(4, 3)) + im*(randn(4, 3) + δ*randn(4, 3))
         U = FTField(U_)
-        p = FTField(4, Complex{Float64})
+        p = FTField(4, Float64)
         get_pert!(U, p)
         @test real.(p.data) == pert.(real.(U_))
         @test imag.(p.data) == pert.(imag.(U_))
