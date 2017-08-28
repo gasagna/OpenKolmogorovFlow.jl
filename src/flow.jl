f41edb389d8b900a62e7a34db40f94839e2e662c
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-08-28 23:53:32 +0100

    better error message

--- src/flow.jl
+++ src/flow.jl
@@ -1,11 +1,9 @@
 export laminarflow, dissrate
 
-"""
-    Returns the vorticity field of the laminar flow.
-"""
+# Returns the vorticity field of the laminar flow.
 function laminarflow(n::Int, Re::Real, kforcing::Int=4)
-    kforcing ≤ n>>1 || 
-        throw(ArgumentError("forcing wave number too large"))
+    0 ≤ kforcing ≤ n>>1  ||
+        throw(ArgumentError("forcing wave number must be in [0, n/2]"))
     Ω = FTField(n)
     Ω[-kforcing, 0] = -Re/kforcing/2
     Ω[ kforcing, 0] = -Re/kforcing/2
