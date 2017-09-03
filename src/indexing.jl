1c6fe1844b33a93e8224078e7dbe46250f8ab019
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-09-03 17:15:49 +0100

    added few indexing functions

--- src/indexing.jl
+++ src/indexing.jl
@@ -9,9 +9,17 @@
 # Return the 'k' or 'j' wave numbers corresponding to storage location 
 # `i`, for a grid with `n` points
 @inline ItoJ(i::Int, n::Int) = div(i-1, n)
-@inline ItoK(i::Int, n::Int) = (k=rem(i-1, n); ifelse(k>n>>1, k-n, k))
+@inline ItoK(i::Int, n::Int) = (k = rem(i-1, n); ifelse(k > n>>1, k-n, k))
 @inline ItoKJ(i, n) = (ItoK(i, n), ItoJ(i, n))
+
+# Return `i`-th column or row where wave numbers `j` or `k` are stored
+@inline JtoI(j::Int) = abs(j) + 1
+@inline KtoI(k::Int, n::Int) = k ≥ 0 ? k + 1 : n + k + 1
 
 # Return conjugate of `val` if `j` is negative. Other symmetries are taken
 # care of by the `KJtoI` function
-@inline rectify(val::Number, j::Int) = ifelse(j<0, conj(val), val)
+@inline rectify(val::Number, j::Int) = j < 0 ? conj(val) : val
+
+# Tuples of j and k wave numbers
+js(n) = NTuple{n>>1+1, Int}(j for j = 0:n>>1)
+ks(n) = NTuple{n, Int}(ItoK(i, n) for i = 1:n)
