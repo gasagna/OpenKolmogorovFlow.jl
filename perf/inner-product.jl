b94a1d7d1dbeb0fde256cea6b865f39741014a45
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-09-03 17:22:19 +0100

    add performance script to inner product

--- perf/inner-product.jl
+++ perf/inner-product.jl
@@ -1,9 +1,10 @@
 using OpenKolmogorovFlow
 using BenchmarkTools
-import MacroTools: postwalk, @capture
 
 n = 100
 u = randn(n, n); U = FFT(Field(u))
 v = randn(n, n); V = FFT(Field(v))
+W = shifted(V, (1, 2))
 
 @btime inner($U, $V)
+@btime inner($U, $W)
