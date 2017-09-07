d4effe2d792ec02c8365806720d78b7964d08bc7
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-09-07 22:14:30 +0100

    modified performance script

--- perf/inner-product.jl
+++ perf/inner-product.jl
@@ -4,7 +4,12 @@ using BenchmarkTools
 n = 100
 u = randn(n, n); U = FFT(Field(u))
 v = randn(n, n); V = FFT(Field(v))
+cache = DistanceCache(64)
 W = shifted(V, (1, 2))
 
 @btime inner($U, $V)
 @btime inner($U, $W)
+
+@btime innerdiff($U, $V)
+@btime innerdiff($U, $W)
+@btime distance!($U, $V, $cache)
