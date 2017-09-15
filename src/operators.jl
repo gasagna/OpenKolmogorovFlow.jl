f640def5d3ee55092764a9a528406977f677df11
Davide Lasagna <davide.lasagna@soton.ac.uk>
2017-09-15 21:22:06 +0100

    improve code style

--- src/operators.jl
+++ src/operators.jl
@@ -1,3 +1,5 @@
+export DiffOperator
+
 struct DiffOperator{n, T, M<:AbstractMatrix{T}} <: AbstractFTOperator{n, T}
     data::M
 end
@@ -8,12 +10,12 @@ Base.@propagate_inbounds @inline Base.getindex(d::DiffOperator, i::Int) = d.data
 function DiffOperator(n::Int, i::Symbol, ::Type{T}=Float64) where {T<:Union{AbstractFloat, Integer}}
     i == :x    && return DiffOperator{n, Complex{T}, Matrix{Complex{T}}}(     Complex{T}[im*j for k=      1:n,    j=0:n>>1])
     i == :y    && return DiffOperator{n, Complex{T}, Matrix{Complex{T}}}(vcat(Complex{T}[im*k for k=      0:n>>1, j=0:n>>1],
-                                                          Complex{T}[im*k for k=-n>>1+1:-1,   j=0:n>>1]))
-    i == :xx   && return DiffOperator{n, T, Matrix{T}}(                       T[-j*j for k=      1:n,    j=0:n>>1])
-    i == :yy   && return DiffOperator{n, T, Matrix{T}}(                  vcat(T[-k*k for k=      0:n>>1, j=0:n>>1],
-                                                                   T[-k*k for k=-n>>1+1:-1,   j=0:n>>1]))
-    i == :xxyy && return DiffOperator{n, T, Matrix{T}}(                       T[-j*j for k=      1:n,    j=0:n>>1] +
-                                                              vcat(T[-k*k for k=      0:n>>1, j=0:n>>1],
-                                                                   T[-k*k for k=-n>>1+1:-1,   j=0:n>>1]))
+                                                                              Complex{T}[im*k for k=-n>>1+1:-1,   j=0:n>>1]))
+    i == :xx   && return DiffOperator{n, T, Matrix{T}}(                                T[-j*j for k=      1:n,    j=0:n>>1])
+    i == :yy   && return DiffOperator{n, T, Matrix{T}}(                           vcat(T[-k*k for k=      0:n>>1, j=0:n>>1],
+                                                                                       T[-k*k for k=-n>>1+1:-1,   j=0:n>>1]))
+    i == :xxyy && return DiffOperator{n, T, Matrix{T}}(                                T[-j*j for k=      1:n,    j=0:n>>1] +
+                                                                                  vcat(T[-k*k for k=      0:n>>1, j=0:n>>1],
+                                                                                       T[-k*k for k=-n>>1+1:-1,   j=0:n>>1]))
     throw(ArgumentError("differentiation not understood, got $i"))
 end
