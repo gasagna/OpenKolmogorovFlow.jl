517f785baaec970eb39de116e3cc08fe07e18047
Davide Lasagna <lasagnadavide@gmail.com>
2018-06-20 21:29:42 +0100

    implemented adjoint code

---
+++ src/linearisation.jl
@@ -0,0 +1,134 @@
+export LinearisedEquation, imex
+
+abstract type AbstractMode end
+abstract type AbstractDirectMode  <: AbstractMode end
+abstract type AbstractAdjointMode <: AbstractMode end
+
+# ~~~ Explicit Term of Linearised Equations ~~~
+struct LinearisedExplicitTerm{n, M<:AbstractMode,        FT<:AbstractFTField,
+                                 F<:AbstractField,       D1<:AbstractFTOperator, 
+                                D2<:AbstractFTOperator, ITT<:InverseFFT!,
+                               FTT<:ForwardFFT!, S, C}
+    FTFStore::Vector{FT} # storage
+      FStore::Vector{F}
+          dx::D1         # operators
+          dy::D1
+           Δ::D2
+       ifft!::ITT        # transforms
+        fft!::FTT
+         sol::S          # forward solution storage
+        cost::C          # forcing to linearised equation
+end
+
+# Outer constructor: TODO: merge with above
+function LinearisedExplicitTerm(n::Int, m::Int, mode::AbstractMode, sol, cost, ::Type{S}, flags::UInt32) where {S}
+    # stupidity check
+    m ≥ n || throw(ArgumentError("`m` must be bigger than `n`, got `n, m= $n, $m`. Are you sure?"))
+
+    # number of fields for storage
+    nF  = mode isa AbstractAdjointMode ? 7 :
+          mode isa AbstractDirectMode  ? 9 : error("wrong mode")
+
+    # complex fields have size `n` but real fields might have larger size `m`
+    FTFStore = [FTField(n, Complex{S}) for i = 1:nF]
+    FStore   = [  Field(m, S)          for i = 1:nF-1]
+
+    # transforms
+    ifft! = InverseFFT!(m, FTFStore[1], flags)
+     fft! = ForwardFFT!(n,   FStore[1], flags)
+
+    # operators
+    dx, dy, Δ = DiffOperator(n, :x, S), DiffOperator(n, :y, S), DiffOperator(n, :xxyy, S)
+
+    # construct object
+    LinearisedExplicitTerm{n, 
+                           typeof(mode),
+                           eltype(FTFStore), 
+                           eltype(FStore), 
+                           typeof(dx), 
+                           typeof(Δ), 
+                           typeof(ifft!), 
+                           typeof(fft!), 
+                           typeof(sol), 
+                           typeof(cost)}(FTFStore, FStore, dx, dy, Δ, ifft!, fft!, sol, cost)
+end
+
+# extract forward solution at current time
+(Eq::LinearisedExplicitTerm{n, FT})(t::Real, Λ::FT, dΛdt::FT, add::Bool=false) where {n, FT<:FTField{n}} = 
+    Eq(t, Eq.sol(Eq.FTFStore[1], t), Λ, dΛdt)
+
+# Adhere to callable interface for IMEXRKCB
+function (Eq::LinearisedExplicitTerm{n, FT})(t::Real, Ω::FT, Λ::FT, dΛdt::FT, add::Bool=false) where {n, FT<:FTField{n}}
+    # extract aliases. We start from one because 1 might be taken for Ω
+                U,    V    = Eq.FTFStore[2], Eq.FTFStore[3]
+                TMP1, TMP2 = Eq.FTFStore[2], Eq.FTFStore[3]
+    dΛdx, dΛdy, dΩdx, dΩdy = Eq.FTFStore[4], Eq.FTFStore[5], Eq.FTFStore[6], Eq.FTFStore[7] 
+                u,    v    = Eq.FStore[1],   Eq.FStore[2]
+                tmp1, tmp2 = Eq.FStore[1],   Eq.FStore[2]
+    dλdx, dλdy, dωdx, dωdy = Eq.FStore[3],   Eq.FStore[4],   Eq.FStore[5],   Eq.FStore[6]
+    dx, dy, Δ = Eq.dx, Eq.dy, Eq.Δ
+
+    mode isa AbstractDirectMode && U′,   V′   = Eq.FTFStore[8], Eq.FTFStore[9]
+
+    # set mean to zero
+    Λ[0, 0] = zero(eltype(Λ))
+
+    # obtain vorticity derivatives
+    dΛdx .= dx .* Λ
+    dΛdy .= dy .* Λ
+    dΩdx .= dx .* Ω
+    dΩdy .= dy .* Ω
+
+    # obtain velocity components. Set mean to zero
+    U .= .- dΩdy ./ Δ; U[0, 0] = zero(eltype(U))
+    V .=    dΩdx ./ Δ; V[0, 0] = zero(eltype(V))
+
+    # inverse transform to physical space into temporaries
+    Eq.ifft!(u,    U)
+    Eq.ifft!(v,    V)
+    Eq.ifft!(dλdx, dΛdx); Eq.ifft!(dωdx, dΩdx)
+    Eq.ifft!(dλdy, dΛdy); Eq.ifft!(dωdy, dΩdy)
+
+    # multiply in physical space
+    tmp1 .= u.*dλdx .+ v.*dλdy;       Eq.fft!(TMP1, tmp1)
+    tmp2 .= dλdx.*dωdy .- dλdy.*dωdx; Eq.fft!(TMP2, tmp2)
+    add == true ? (dΛdt .+= .- TMP1 .- TMP2 ./ Δ) : (dΛdt  .= .- TMP1 .- TMP2 ./ Δ)
+
+    # add forcing from cost gradient
+    Eq.cost(dΛdt, Ω)
+
+    return nothing
+end
+
+
+# ~~~ SOLVER OBJECT FOR THE ADJOINT EQUATIONS ~~~
+struct AdjointEquation{n, IT<:ImplicitTerm{n}, ET<:LinearisedExplicitTerm{n}}
+    imTerm::IT
+    exTerm::ET
+end
+
+# outer constructor: main entry point
+function AdjointEquation(n::Int,
+                         Re::Real,
+                         sol,             # the forward solution
+                         cost,            # cost gradient function
+                         kforcing::Int=4;
+                         numtype::Type{S}=Float64,
+                         flags::UInt32=FFTW.PATIENT,
+                         dealias::Bool=true) where {S<:Real}
+    iseven(n) || throw(ArgumentError("`n` must be even, got $n"))
+    m = dealias == true ? even_dealias_size(n) : n
+    imTerm = ImplicitTerm(n, -Re, S)
+    exTerm = LinearisedExplicitTerm(n, m, sol, cost, S, flags)
+    AdjointEquation{n, typeof(imTerm), typeof(exTerm)}(imTerm, exTerm)
+end
+
+# evaluate right hand side of governing equations
+(eq::AdjointEquation{n})(t::Real, Λ::FTField{n}, dΛdt::FTField{n}) where {n} =
+    (A_mul_B!(dΛdt, eq.imTerm, Λ); eq.exTerm(t, Λ, dΛdt, true))
+
+# obtain two components
+imex(eq::AdjointEquation) = (eq.imTerm, eq.exTerm)
+
+# multiply in physical space
+u .= u.*dω′dx .+ v.*dω′dy.+ u′.*dωdx .+ v′.*dωdy
---
+++ tests/test_adjoint_gradient.jl
@@ -0,0 +1,83 @@
+using OpenKolmogorovFlow
+using IMEXRKCB
+using Base.Test
+
+# We use the adjoint code to verify the identity 
+#
+# (Λ_T, L Ω′_0) = (L⁺Λ_T, Ω′_0)
+# 
+# where the subscripts _T and _0 denote the final and initial 
+# states of the adjoint variables Λ and perturbation Ω′. The 
+# operator L is the linearisation of the forward map, while L⁺ 
+# is its adjoint, from integration by parts in space and time.
+# The adjoint operator L⁺ runs backward in time. Here we check
+# that the identity is verified up to a certain precision. 
+# The identity cannot be satisfied exactly, because we use 
+# interpolation of the state variables to rune the adjoint, 
+# whereas the variational equations are run along with the 
+# forward equations.
+@testset "adjoint                           " begin
+    
+    # setup
+    const Δt = 0.015    # time step
+    const n  = 64       # problem size
+    const Re = 20.0     # reynolds number
+    const kforcing = 4  # forcing wave number
+    const T = 5         # time horizon
+
+    for dealias in [false, true]
+        for impl in [IMEXRK3R2R(IMEXRKCB3c, false),
+                     IMEXRK3R2R(IMEXRKCB3e, false),
+                     IMEXRK4R3R(IMEXRKCB4,  false)]
+
+            # ~~~ FORWARD PROBLEM ~~~
+            # random initial conditions
+            srand(0)
+
+            # state
+            Ωs = FFT(Field(0.1*randn(n, n))); Ωs[0, 0] = 0
+
+            # perturbation in a single direction
+            Ωp = FTField(n); Ωp[1, 2] = 1.0
+
+            # augmented state
+            Ω = VariationalFTField(Ωs, copy(Ωp));
+
+            # define forward and variational equation
+            L_forw, N_forw = imex(ForwardEquation(n, Re, kforcing, TangentMode(); dealias=false))
+
+            # define forward integrator
+            f_forw = integrator(N_forw, L_forw, IMEXRKScheme(impl, Ω), 0.01)
+
+            # storage for forward solution only
+            forw_sol = Monitor(Ω, Ω->copy(state(Ω)));
+
+            # now store forward solution
+            f_forw(Ω, (0, T), forw_sol);
+
+            # calculate 
+            LHS = dot(state(Ω), prime(Ω)) 
+
+            # ~~~ ADJOINT ~~~
+            cost(dΛdt::FTField, Ω::FTField) = nothing
+
+            # define backward equations
+            L_back, N_back = imex(AdjointEquation(n, Re, forw_sol, cost, kforcing; dealias=true))
+
+            # define backward integrator
+            f_back = integrator(N_back, L_back, IMEXRKScheme(impl, Ωs), 0.01);
+
+            # monitor the adjoint state
+            mon = Monitor(Ωs, copy);
+
+            # march backwards the final state
+            f_back(Ωs, (T, 0), mon);
+
+            # calculate 
+            RHS = dot(Ωs, Ωp)
+
+            Δ = abs(LHS - RHS)/abs(RHS)
+            @test Δ < 1e-6
+        end
+    end
+end
