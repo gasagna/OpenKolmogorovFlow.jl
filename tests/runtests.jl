ddcd7effa815312094680b4c5fc0e26065ea434c
Davide Lasagna <lasagnadavide@gmail.com>
2019-01-02 18:14:33 +0000

    refactor test files

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -4,21 +4,19 @@ import Random: seed!
 import LinearAlgebra: dot, norm
 using Flows
 
-include("test_field.jl")
-include("test_operators.jl")
-include("test_ftfield.jl")
-include("test_broadcast.jl")
-include("test_indexing.jl")
-include("test_norms.jl")
-include("test_system.jl")
-include("test_allocation.jl")
-include("test_flow.jl")
-
-# include("test_fft.jl")        # FIXME
-# include("test_spectra.jl")
-# include("test_distance.jl")
-# include("test_shifts.jl")
-# include("test_tangent.jl")
-# include("test_variational.jl")
-# include("test_adjoint_identity.jl")
 # include("test_adjoint_allocations.jl")
+# include("test_adjoint_identity.jl")
+include("test_allocation.jl")
+include("test_broadcast.jl")
+# include("test_distance.jl")
+# include("test_fft.jl")        # FIXME
+include("test_field.jl")
+include("test_flow.jl")
+include("test_ftfield.jl")
+include("test_indexing.jl")
+include("test_nonlinear.jl")
+include("test_norms.jl")
+include("test_operators.jl")
+# include("test_shifts.jl")
+# include("test_spectra.jl")
+include("test_tangent.jl")
